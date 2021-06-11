1a6b1eaa2e7961ab46d6a26b31e7f827dc07be2f
dheluffy <85587722+dheluffy@users.noreply.github.com>
2021-06-11 21:18:17 +0700

    Add files via upload

---
+++ estilonewserver.php
@@ -0,0 +1,34 @@
+
+<?xml version="1.0" encoding="UTF-8" ?>    
+<teamtalk>
+                <host>
+                    <name>ESTILO NEW</name>
+                    <address>103.140.55.77</address>
+                    <tcpport>22222</tcpport>
+                    <udpport>22222</udpport>
+                    <encrypted>true</encrypted>
+                    <auth>
+                        <username></username>
+                        <password></password>
+                        </auth>
+                        <join>
+                            <channel>/</channel>
+                            <password></password>
+                            </join>
+                            </host>
+                            <host>
+                            <name>ESTILO-V2</name>
+                            <address>103.157.116.41</addres>
+                            <tcpport>11111</tcpport>
+                            <udpport>11111</udpport>
+                            <encrypted>true</encrypted>
+                            <auth>
+                                <username></username>
+                                <password></password>
+                                </auth>
+                                <join>
+                                    <channel>/</channel>
+                                    <password></password>
+                                    </join>
+                                    </host>
+                                    </teamtalk>
